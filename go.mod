mcpherrinm/certigo

Initial project

go.mod
@@ -1,0 +1,3 @@
+module github.com/mcpherrinm/certigo
+
+go 1.24.2

certcat/certcat

go.mod
@@ -1,0 +1,3 @@
+module github.com/certcat/certcat
+
+go 1.24.2