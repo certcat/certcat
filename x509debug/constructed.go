d900c338107995c2726cf075cdd7b575a6bbaa21
Matthew McPherrin <git@mcpherrin.ca>
2025-08-03 18:03:11 -0400

    Pull out an explicit ParsablePtr type
    Cleans up the type signature a bit

--- x509debug/constructed.go
+++ x509debug/constructed.go
@@ -12,12 +12,14 @@ type Parsable interface {
 	Parse(data *cryptobyte.String) error
 }
 
-// ParseSequenceOf parses an ASN.1 SequenceOf
-// Tag should usually be asn1.Sequence, except when using implicit encoding.
-func ParseSequenceOf[T any, PT interface {
+type ParsablePtr[T any] interface {
 	*T
 	Parsable
-}](data *cryptobyte.String, tag asn1.Tag) ([]T, error) {
+}
+
+// ParseSequenceOf parses an ASN.1 SequenceOf
+// Tag should usually be asn1.Sequence, except when using implicit encoding.
+func ParseSequenceOf[T any, PT ParsablePtr[T]](data *cryptobyte.String, tag asn1.Tag) ([]T, error) {
 	var sequenceOf cryptobyte.String
 	if !data.ReadASN1(&sequenceOf, tag) {
 		return nil, errors.New("failed to parse ASN.1 sequence")
