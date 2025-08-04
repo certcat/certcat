c07185f49734321788b8596912a091153a973ad4
Matthew McPherrin <git@mcpherrin.ca>
2025-08-03 22:25:59 -0400

    Include actual type in error message from ParseSequenceOf

--- x509debug/constructed.go
+++ x509debug/constructed.go
@@ -1,7 +1,7 @@
 package x509debug
 
 import (
-	"errors"
+	"fmt"
 
 	"golang.org/x/crypto/cryptobyte"
 	"golang.org/x/crypto/cryptobyte/asn1"
@@ -15,12 +15,12 @@ type Parsable[T any] interface {
 // ParseSequenceOf parses an ASN.1 SequenceOf
 // Tag should usually be asn1.Sequence, except when using implicit encoding.
 func ParseSequenceOf[T any, PT Parsable[T]](data *cryptobyte.String, tag asn1.Tag) ([]T, error) {
+	var ret []T
+
 	var sequenceOf cryptobyte.String
 	if !data.ReadASN1(&sequenceOf, tag) {
-		return nil, errors.New("failed to parse ASN.1 sequence")
+		return nil, fmt.Errorf("failed to parse %T", ret)
 	}
-
-	var ret []T
 
 	for !sequenceOf.Empty() {
 		var t T
