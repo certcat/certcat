99ff99e3405ad71b4252b5c55650e495f89c0053
Matthew McPherrin <git@mcpherrin.ca>
2025-08-09 17:43:07 -0400

    Add a ParseSequence3
    Expand ParseSequence2, and rewrite it to use the new method

--- x509debug/constructed.go
+++ x509debug/constructed.go
@@ -37,31 +37,51 @@ func ParseSequenceOfTagged[T any, PT Parsable[T]](data *cryptobyte.String, tag a
 	return ret, nil
 }
 
+// Empty parses zero bytes
+type Empty struct{}
+
+func (e Empty) Parse(_ *cryptobyte.String) error {
+	return nil
+}
+
 // ParseSequence2 parses a SEQUENCE of two values.
-func ParseSequence2[T1 any, T2 any, PT1 Parsable[T1], PT2 Parsable[T2]](data *cryptobyte.String) (T1, T2, error) {
+func ParseSequence2[T1, T2 any, PT1 Parsable[T1], PT2 Parsable[T2]](data *cryptobyte.String) (T1, T2, error) {
+	r1, r2, _, err := ParseSequence3[T1, T2, Empty, PT1, PT2, *Empty](data)
+	return r1, r2, err
+}
+
+// ParseSequence3 parses a SEQUENCE of three values.
+func ParseSequence3[T1, T2, T3 any, PT1 Parsable[T1], PT2 Parsable[T2], PT3 Parsable[T3]](data *cryptobyte.String) (T1, T2, T3, error) {
 	var r1 T1
 	var r2 T2
+	var r3 T3
 
 	var sequence cryptobyte.String
 	if !data.ReadASN1(&sequence, asn1.SEQUENCE) {
-		return r1, r2, fmt.Errorf("failed to parse sequence {%T %T}", r1, r2)
+		return r1, r2, r3, fmt.Errorf("failed to parse sequence {%T %T %T}", r1, r2, r3)
 	}
 
 	var pt1 PT1 = &r1
 	err := pt1.Parse(&sequence)
 	if err != nil {
-		return r1, r2, fmt.Errorf("parsing %T: %w", r1, err)
+		return r1, r2, r3, fmt.Errorf("parsing %T: %w", r1, err)
 	}
 
 	var pt2 PT2 = &r2
 	err = pt2.Parse(&sequence)
 	if err != nil {
-		return r1, r2, fmt.Errorf("parsing %T: %w", r2, err)
+		return r1, r2, r3, fmt.Errorf("parsing %T: %w", r2, err)
+	}
+
+	var pt3 PT3 = &r3
+	err = pt3.Parse(&sequence)
+	if err != nil {
+		return r1, r2, r3, fmt.Errorf("parsing %T: %w", r2, err)
 	}
 
 	if !sequence.Empty() {
-		return r1, r2, fmt.Errorf("trailing data after {%T %T}", r1, r2)
+		return r1, r2, r3, fmt.Errorf("trailing data after {%T %T %T}", r1, r2, r3)
 	}
 
-	return r1, r2, nil
+	return r1, r2, r3, nil
 }
